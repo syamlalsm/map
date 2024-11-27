8a8c50cef3a0acddd0db443cc27933729aebf417
syamlalsm <syamklr10@gmail.com>
2024-11-27 18:38:39 +0530

    new change

--- map.py
+++ map.py
@@ -1,5 +1,12 @@
 def start_with_A(s):
 	return s[0]=="A"
 fruit=["Apple","Banana","Apricot","Orange"]
+fruit=["Apple","Banana","Apricot","Orange"]
+fruit=["Apple","Banana","Apricot","Orange"]
+fruit=["Apple","Banana","Apricot","Orange"]
+fruit=["Apple","Banana","Apricot","Orange"]
+fruit=["Apple","Banana","Apricot","Orange"]
+fruit=["Apple","Banana","Apricot","Orange"]
+fruit=["Apple","Banana","Apricot","Orange"]
 map_object=map(starts_with_A,fruit)
 print(list(map_object))
